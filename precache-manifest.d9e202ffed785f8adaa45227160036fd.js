788f085648034d04770662432c40446d7c41c9a1
Kristoffer <kb@ristolina.com>
2020-04-01 15:01:29 +0200

    Updates

---
+++ precache-manifest.d9e202ffed785f8adaa45227160036fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07daec680933a715761118670e329813",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "035155ff21344eb7f381",
+    "url": "/dev/static/css/2.221380cf.chunk.css"
+  },
+  {
+    "revision": "fbe48277cdd84b270bc2",
+    "url": "/dev/static/css/main.88b21ea9.chunk.css"
+  },
+  {
+    "revision": "035155ff21344eb7f381",
+    "url": "/dev/static/js/2.41075e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dev/static/js/2.41075e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe48277cdd84b270bc2",
+    "url": "/dev/static/js/main.223ae1e3.chunk.js"
+  },
+  {
+    "revision": "744f77b8dc289c54fbd7",
+    "url": "/dev/static/js/runtime-main.3e372214.js"
+  },
+  {
+    "revision": "42fd2d744343d3a64ce296b0fcc475e3",
+    "url": "/dev/static/media/background.42fd2d74.jpg"
+  },
+  {
+    "revision": "8c7c6f3d57baa683ec3b9ecea428f2e0",
+    "url": "/dev/static/media/brackets.8c7c6f3d.svg"
+  },
+  {
+    "revision": "5291b74f9407cae38a2f48f873b492de",
+    "url": "/dev/static/media/code.5291b74f.svg"
+  },
+  {
+    "revision": "4de2eb4d518c8ce43d9425e5d3260691",
+    "url": "/dev/static/media/ristolina-logo_mono-light.4de2eb4d.svg"
+  },
+  {
+    "revision": "8baa2610a040797b003f166bbc323cf9",
+    "url": "/dev/static/media/terminal.8baa2610.svg"
+  }
+]);
